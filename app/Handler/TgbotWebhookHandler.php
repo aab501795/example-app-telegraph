71e183fb54907919fc89fff2343776844e377f0e
aab501795 <115061026+aab501795@users.noreply.github.com>
2022-11-25 23:20:04 +0800

    更新 TgbotWebhookHandler

--- app/Handler/TgbotWebhookHandler.php
+++ app/Handler/TgbotWebhookHandler.php
@@ -2,10 +2,31 @@
 
 namespace App\Handler;
 
+use DefStudio\Telegraph\Enums\ChatActions;
+use DefStudio\Telegraph\Facades\Telegraph;
+use DefStudio\Telegraph\Keyboard\ReplyButton;
+use DefStudio\Telegraph\Keyboard\ReplyKeyboard;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Stringable;
+
 class TgbotWebhookHandler extends \DefStudio\Telegraph\Handlers\WebhookHandler
 {
+    public function start()
+    {
+        Log::debug('WebhookHandler-start-'.time());
+        $this->chat->markdown("*Hello* 欢迎使用!")->send();
+    }
+
     public function hi()
     {
+        Log::debug('WebhookHandler-hi-'.time());
         $this->chat->markdown("*Hi* happy to be here!")->send();
+    }
+
+    protected function handleChatMessage(Stringable $text): void
+    {
+        Log::debug('WebhookHandler-handleChatMessage-'.time());
+        //in this example, a received message is sent back to the chat
+        $this->chat->html("Received: $text")->send();
     }
 }
