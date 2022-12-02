09b7574ebbf03a7a570ae6412c55e11c6588e0f5
aab501795 <115061026+aab501795@users.noreply.github.com>
2022-12-03 01:17:36 +0800

    更新 TgbotWebhookHandler，composer require "overtrue/laravel-lang:~6.0"

--- app/Handler/TgbotWebhookHandler.php
+++ app/Handler/TgbotWebhookHandler.php
@@ -4,13 +4,45 @@ namespace App\Handler;
 
 use DefStudio\Telegraph\Enums\ChatActions;
 use DefStudio\Telegraph\Facades\Telegraph;
+use DefStudio\Telegraph\Handlers\WebhookHandler;
+use DefStudio\Telegraph\Keyboard\Button;
+use DefStudio\Telegraph\Keyboard\Keyboard;
 use DefStudio\Telegraph\Keyboard\ReplyButton;
 use DefStudio\Telegraph\Keyboard\ReplyKeyboard;
+use DefStudio\Telegraph\Models\TelegraphBot;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Stringable;
 
-class TgbotWebhookHandler extends \DefStudio\Telegraph\Handlers\WebhookHandler
+class TgbotWebhookHandler extends WebhookHandler
 {
+
+    public function setting(){
+        $this->chat->message('hello world')
+            ->keyboard(Keyboard::make()->buttons([
+                Button::make('Delete')->action('delete')->param('id', '42'),
+                Button::make('open')->url('https://test.it'),
+                Button::make('Web App')->webApp('https://web-app.test.it'),
+            ]))->send();
+    }
+
+    public function addCommand(){
+        //...
+
+        $this->bot->registerCommands([
+            'setting' => 'command 1111 description',
+            'addCommand' => 'command 2222 description'
+        ])->send();
+        $this->chat->markdown("设置成功")->send();
+    }
+
+    public function addbot(){
+        $bot = TelegraphBot::create([
+            'token' => '5803298657:AAGkCGQkEc17IJEP4ms6G8LHex2GLlP3ogc',
+            'name' => '非人类',
+        ]);
+        $bot->registerWebhook()->send();
+    }
+
     public function start()
     {
         Log::debug('WebhookHandler-start-'.time());
